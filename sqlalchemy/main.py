6479a000841b2bf6f77d9e1343dba96db76f5a40
Akshai <72917775+Akshai-2909@users.noreply.github.com>
2022-03-06 12:28:49 +0530

    Delete sqlalchemy directory

--- sqlalchemy/main.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-
-##Engine is a starting point, Where it reference both a dialect and pool which together interpret DBAPI's module functions as well as db
-
-engine = create_engine('sqlite+pysqlite:///./cookies.db',echo=True)
-# Produce an engine object based on the url
-
-connection = engine.connect()
-# opens a connection to the database
